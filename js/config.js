d506416f7eb6224d9e8ef1bbc61bc8126b1ecda8
Mclient01 <Mclient01@outlook.com>
2026-06-13 18:24:24 +0100

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// ============================================================
-// سوق بلادي - Souq Bladi - Configuration File
-// ============================================================
-// ⚠️ ملف التكوين - Supabase Configuration
-// ⚠️ Configuration File - Supabase Configuration
-
-// إعدادات Supabase / Supabase Configuration
-const SUPABASE_URL = 'https://rdsnxvksnlfvbslcbljn.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkc254dmtzbmxmdmJzbGNibGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMzE5NTYsImV4cCI6MjA5NTgwNzk1Nn0.n481Gt0XOAFbF3pu46ql3EX9aQV2T73Ou5OjvvSHDD0'
